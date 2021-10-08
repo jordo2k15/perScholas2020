bc049adb4e7d208d4f800d7b7e58d0c0962af005
jordo2k15 <john.ordonez1996@gmail.com>
2021-10-08 12:36:52 -0400

    Intro-to-Java

---
+++ Zoo.java
@@ -0,0 +1,9 @@
+public class Zoo {
+    public static void main(String[] args) {
+        int animals = 12;
+        String species = "zebra";
+        String zooDescription = "Our Zoo has " + animals + " " + species + "s!";
+        System.out.println(zooDescription);
+        
+    }
+}
