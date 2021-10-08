eeba71315648682a397ef1ecb2dd2afb6cf7f5b1
jordo2k15 <john.ordonez1996@gmail.com>
2021-10-08 12:43:35 -0400

    Comments

--- Zoo.java
+++ Zoo.java
@@ -1,9 +1,15 @@
+// Topic: String Concatenation 
+// The '+' operator, which we use for adding numbers together, can be used to concatenate Strings
+// This allows us to use it to join two Strings together!
+// We can even use a primitive datatype as the second variable to concatenate, and Java will 
+// intelligently make it a String first:
+
 public class Zoo {
     public static void main(String[] args) {
         int animals = 12;
         String species = "zebra";
         String zooDescription = "Our Zoo has " + animals + " " + species + "s!";
-        System.out.println(zooDescription);
+        System.out.println(zooDescription); // print results 'Our zoo has 12 zebras!''
         
     }
 }
